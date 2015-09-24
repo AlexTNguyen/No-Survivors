4d2c86084efbb6c33fe25067d0f9982ce0424d56
kcrowe01 <Kenneth.Crowell@tufts.edu>
2015-09-24 18:25:12 -0400

    created html and js files for game

---
+++ game.js
@@ -0,0 +1,15 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, "game", {preload:preload, update:update, create:create});
+
+function preload() 
+{
+	
+}
+
+function create()
+{
+	
+}
+function update()
+{
+	
+}
