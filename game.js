c4e85074537f11607de0063f75243874a00b1279
Alex Nguyen <alex.nguyen@tufts.edu>
2015-09-24 19:05:21 -0400

    added wizard sprite to game

--- game.js
+++ game.js
@@ -2,12 +2,12 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, "game", {preload:preload, upda
 
 function preload() 
 {
-	
+	game.load.spritesheet('wizard', 'sprites/wizard.png', 40, 60);
 }
 
 function create()
 {
-	
+	game.add.sprite(0, 0, 'wizard');
 }
 function update()
 {
