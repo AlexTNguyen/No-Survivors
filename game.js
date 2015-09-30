dbfc02983fac102ea83c23376f8410429e095c2c
kcrowe01 <Kenneth.Crowell@tufts.edu>
2015-09-30 19:42:00 -0400

    added player animations + ground and sky

--- game.js
+++ game.js
@@ -1,15 +1,29 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, "game", {preload:preload, update:update, create:create});
 
+var wizard 
+
 function preload() 
 {
-	game.load.spritesheet('wizard', 'sprites/wizard.png', 40, 60);
+	game.load.spritesheet('wizard', 'sprites/wiz1.png', 43, 50);
+	game.load.spritesheet('ground', 'sprites/Ground&Stone/Ground&Stone/Stone/ground5.png');
+	game.load.spritesheet('sky', 'sprites/sky.png');
 }
 
 function create()
 {
-	game.add.sprite(0, 0, 'wizard');
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	game.add.sprite(0,0,'sky');
+	cursors = game.input.keyboard.createCursorKeys();
+	wizard = new Wizard(game, game.world.centerX, 550);
+	stones = game.add.group();
+	stones.enableBody = true;
+	for(var i = 0; i < 10; i++){
+		var ground = stones.create(i * 125, 550, 'ground');
+		ground.scale.setTo(1, 0.5);
+		ground.body.immovable = true;
+	}
 }
 function update()
 {
-	
+	game.physics.arcade.collide(wizard, stones);
 }
---
+++ wizard.js
@@ -0,0 +1,62 @@
+Wizard.prototype = Object.create(Phaser.Sprite.prototype);
+
+Wizard.prototype.constructor = Wizard;
+
+Wizard.prototype.force = {x:0.0, y:0.0}
+
+function Wizard(game, x, y){
+	Phaser.Sprite.call(this, game, x, y, 'wizard');
+	this.anchor.setTo(0.5, 1);
+	game.physics.enable(this, Phaser.Physics.ARCADE);
+	this.body.allowRotation = false;
+	this.body.gravity.y = 300;
+	this.body.collideWorldNounds = true;
+	this.animations.add('move', [0, 1, 2, 3], 10, false);
+	this.animations.add('cast', [4, 5, 6], 10, false);
+	game.add.existing(this);
+}
+
+Wizard.prototype.update = function(){
+	wizard.body.velocity.x = 0;
+
+	if (cursors.left.isDown)
+	{
+		wizard.scale.x = 1;
+		wizard.animations.play('move');
+		wizard.body.velocity.x = -150;
+	}
+	else if (cursors.right.isDown)
+	{
+		wizard.animations.play('move');
+		//flip sprite along y axis
+		wizard.scale.x = -1;
+		wizard.body.velocity.x = 150;
+	}
+	//press down arrow to test play animation
+	//eventually this will only be played when the player
+	//inputs the sequence for an attack
+	else if (cursors.down.isDown) 
+	{
+		wizard.animations.play('cast');
+	}
+	else
+	{ 
+		//if not casting then stop animation
+		if((wizard.frame != 4) && (wizard.frame != 5) && (wizard.frame != 6)){
+			wizard.animations.stop();
+			wizard.frame = 0;
+		}
+		//allow casting animation to finish before going back to
+		//idle state
+		else if(wizard.frame == 6){
+			game.time.events.add(200, function(){
+				wizard.animations.stop();
+				wizard.frame = 0;
+			});
+		}
+	}
+	if (cursors.up.isDown && wizard.body.touching.down)
+	{
+		wizard.body.velocity.y = -350;
+	}
+}
