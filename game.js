72725ab25dd5f9b787d55399d56992e4eaf6d6ce
Alex Nguyen <alex.nguyen@tufts.edu>
2015-10-05 14:30:29 -0400

    Added more zombie spawn + follows wizard

--- game.js
+++ game.js
@@ -3,28 +3,27 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, "game", {preload:preload, upda
 var wizard;
 var plasma;
 var casting;
-var zombie;
 
 function preload() 
 {
 	game.load.spritesheet('wizard', 'sprites/wiz1.png', 43, 49);
 	game.load.spritesheet('ground', 'sprites/Ground&Stone/Ground&Stone/Stone/ground5.png');
-	game.load.spritesheet('sky', 'sprites/sky.png');
+	game.load.spritesheet('dungeon', 'sprites/dungeon.jpg');
 	game.load.spritesheet('plasma', 'sprites/plasma.png', 32, 28);
-	game.load.spritesheet('zombie', 'sprites/zombie.png', 72, 75, 16);
+	game.load.spritesheet('zombie', 'sprites/zombie.png', 73, 77, 20);
 }
 
 function create()
 {
 	game.physics.enable(Phaser.Physics.ARCADE);
-	game.add.sprite(0,0,'sky');
+	game.add.sprite(0,0,'dungeon');
 	cursors = game.input.keyboard.createCursorKeys();
 	plasmas = game.add.group();
-	//zombies = game.add.group();
+	zombies = game.add.group();
 	wizard = new Wizard(game, game.world.centerX, 550);
 	stones = game.add.group();
 	
-	//zombies.enableBody = true;
+	zombies.enableBody = true;
 	stones.enableBody = true;
 	for(var i = 0; i < 10; i++){
 		var ground = stones.create(i * 125, 550, 'ground');
@@ -32,23 +31,34 @@ function create()
 		ground.body.immovable = true;
 	}
 
-	zombie = new Zombie(game, 50, 400);
-	console.log("Zombie %d", zombie.body.sourceWidth);
-	console.log("Wizard %d", wizard.body.sourceWidth);
+	game.time.events.loop(3000, createZombie, this);
+	//zombie = new Zombie(game, 50, 400);
+}
+
+function createZombie () {
+	var random = Math.floor(Math.random() * 2);
+	if(random == 0) {
+		zombie = new Zombie(game, game.world.width - 50, 400);
+		zombies.add(zombie);
+	}
+	else {
+	 	zombie = new Zombie(game, 50, 400);
+	 	zombies.add(zombie);
+	}
 }
 
 function update()
 {
 	game.physics.arcade.collide(wizard, stones);
-	game.physics.arcade.collide(zombie, stones);
-	game.physics.arcade.overlap(zombie, plasmas, die, null);	
+	game.physics.arcade.collide(zombies, stones);
+	game.physics.arcade.overlap(zombies, plasmas, die, null);
 	plasmas.forEach(function(plasma) {
 		plasma.animations.play('plasma');
 	});
 }
 
 function die(zombie, plasma) {
-    zombie.body.velocity.x = 0;
-    plasma.body.velocity.x = 0;
+    zombie.faint(); 
+    plasma.destroy();
 }
 
--- zombie.js
+++ zombie.js
@@ -5,7 +5,6 @@ Zombie.prototype.constructor = Zombie;
 Zombie.prototype.force = {x:0.0, y:0.0};
 
 function Zombie(game, x, y){
-	//zombie = group.create(x, y, 'zombie');
 	Phaser.Sprite.call(this, game, x, y, 'zombie');
 	//this.anchor.setTo(0.5, 1);
 	//zombie.scale.setTo(1, 1);
@@ -13,15 +12,23 @@ function Zombie(game, x, y){
 	this.body.gravity.y = 300;
 	this.body.allowRotation = false;
 	this.animations.add('move', [4, 5, 6, 7], 5, true);
-	game.add.existing(this);
+	this.animations.add('die', [12, 16, 17, 18], 5, false);
+	//game.add.existing(zombie);
 	this.animations.play('move');
-	this.body.velocity.x = 150;
-	this.scale.x = -1;
+	//this.body.velocity.x = 150;
 };
-
-// Zombie.prototype.collide = function(){
-
-// }
 
 Zombie.prototype.update = function () {
+	if(this.body.x < wizard.body.x)
+		this.scale.x = -1;
+	else
+		this.scale.x = 1;
+	game.physics.arcade.moveToObject(this, wizard, 100);
 };
+
+Zombie.prototype.faint = function() {
+	//console.log("hello");
+	this.body.velocity.x = 0; 
+	this.animations.play('die');
+	//this.destroy();
+}
